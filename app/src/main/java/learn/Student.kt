1712087e445134e57a94d59c14618145bfbc5f66
tangshg <tangshg@yeah.net>
2022-09-23 16:24:43 +0800

    多态

--- app/src/main/java/learn/Student.kt
+++ app/src/main/java/learn/Student.kt
@@ -4,11 +4,19 @@ package learn
 //子类中的构造函数必须调用父类的构造函数
 
 fun main(){
-    var student = Student("tang",22)
+    var student = Student("girl",100)
 
+    student.name = "tang"
+    student.age = 22
     doStudy(student)
+
+    //普通对接口的实现
+    student.doHomeWork()
+    student.readBook()
+
 }
 
+//面向接口编程，多态
 fun doStudy(student : Study){
     student.doHomeWork()
     student.readBook()
