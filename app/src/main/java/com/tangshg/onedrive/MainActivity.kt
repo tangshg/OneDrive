aa30f357deb98680ee9ba70f68cd7384262cd7dd
tangshg <37105296+tangshg@users.noreply.github.com>
2022-09-27 20:39:14 +0800

    注册 menu 的点击事件

--- app/src/main/java/com/tangshg/onedrive/MainActivity.kt
+++ app/src/main/java/com/tangshg/onedrive/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tangshg.onedrive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 
@@ -19,9 +20,22 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
+    // 这里只用来加载 menu 布局
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main,menu)
+        menuInflater.inflate(R.menu.main, menu)
+        //这里为什么可以直接使用getMenuInflater()，而不使用对象
         return true
     }
+
+    //这里注册 menu 的点击事件
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.add_item ->
+                Toast.makeText(this,"add",Toast.LENGTH_LONG).show()
+            R.id.remove_item ->
+                Toast.makeText(this,"remove",Toast.LENGTH_LONG).show()
+        }
+        return true
+    }
+
 }
