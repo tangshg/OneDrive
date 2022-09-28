4db4e68bd51be9db0dfb3547d5238869ddde56b4
tangshg <37105296+tangshg@users.noreply.github.com>
2022-09-28 20:02:57 +0800

    隐式 Intent

--- app/src/main/java/com/tangshg/onedrive/MainActivity.kt
+++ app/src/main/java/com/tangshg/onedrive/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.tangshg.onedrive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
+import com.tangshg.onedrive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /* 原生方法加载布局
         setContentView(R.layout.activity_main)
+
 
         val button1 :Button = findViewById(R.id.button1)
 
@@ -18,6 +23,27 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"button1",Toast.LENGTH_LONG).show()
 
         }
+
+         */
+
+        //利用 ViewBinding 加载布局
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button1.setOnClickListener{
+           Toast.makeText(this,"button 1",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.button2.setOnClickListener{
+            var intent: Intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button3.setOnClickListener{
+            val intent : Intent = Intent("com.example.activitytest.ACTION_START")
+            startActivity(intent)
+            Toast.makeText(this,"隐式 intent " ,Toast.LENGTH_SHORT).show()
+        }
+
 
     }
     // 这里只用来加载 menu 布局
