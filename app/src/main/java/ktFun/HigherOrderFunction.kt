f0bd1f2a730da1bba9890746bff9f7cf9ad27ab4
tangshg <37105296+tangshg@users.noreply.github.com>
2022-10-03 23:10:56 +0800

    补充高阶函数中的 lambda 表达式

--- app/src/main/java/ktFun/HigherOrderFunction.kt
+++ app/src/main/java/ktFun/HigherOrderFunction.kt
@@ -16,22 +16,36 @@ fun minus (num1: Int,num2 :Int):Int{
 
 //使用高阶函数
 
-fun main(){
+fun main() {
     val n1 = 100
     val n2 = 200
 
-    val result1 = num1AndNum2(n1,n2,::plus)
-    val result2 = num1AndNum2(n1,n2,::minus)
-            println("plus $result1")
+    val result1 = num1AndNum2(n1, n2, ::plus)
+    val result2 = num1AndNum2(n1, n2, ::minus)
+    println("plus $result1")
     println("minus $result2")
 
     //使用 lambda 表达式
-    val result3 = num1AndNum2(n1,n2){n1,n2->
-        n1+n2
+    val result3 = num1AndNum2(n1, n2) { n1, n2 ->
+        n1 + n2
     }
-    val result4 = num1AndNum2(n1,n2){n1,n2->
-        n1-n2
+    val result4 = num1AndNum2(n1, n2) { n1, n2 ->
+        n1 - n2
     }
     println("lambda plus $result3")
     println("lambda minus $result4")
+
+    val lam = { n1: Int, n2: Int -> n1 + n2 }
+    val result5 = num1AndNum2(n1, n2, lam)
+    println("$result5")
+    val result6 = num1AndNum2(n1, n2, { n1: Int, n2: Int -> n1 + n2 })
+    println("$result6")
+    val result7 = num1AndNum2(n1, n2) { n1, n2 ->
+        n1 + n2
+    }
+    println("$result7")
+    val result8 = num1AndNum2(n1, n2, {n1,n2->n1+n2})
+    println("$result8")
+
 }
+
