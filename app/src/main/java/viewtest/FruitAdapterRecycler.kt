7e1b87be52479f9be02b00d7c8400cae08da6022
tangshg <37105296+tangshg@users.noreply.github.com>
2022-10-19 20:48:55 +0800

    为 RecyclerView 注册点击事件

--- app/src/main/java/viewtest/FruitAdapterRecycler.kt
+++ app/src/main/java/viewtest/FruitAdapterRecycler.kt
@@ -5,10 +5,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.tangshg.onedrive.R
 
-class FruitAdapterRecycler(val fruitList: List<Fruit>):
+class FruitAdapterRecycler(private val fruitList: List<Fruit>):
     RecyclerView.Adapter<FruitAdapterRecycler.ViewHolder>() {
         //这里传入 view 是 recycler 子项的最外层布局
         inner class ViewHolder(view : View):RecyclerView.ViewHolder(view){
@@ -17,8 +18,19 @@ class FruitAdapterRecycler(val fruitList: List<Fruit>):
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //view 来源，使用 LayoutInflater 加载
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item,parent,false)
-        return ViewHolder(view)
+
+        val viewHolder = ViewHolder(view)
+
+        //为 RecyclerView 注册点击事件
+        viewHolder.itemView.setOnClickListener{
+            Toast.makeText(parent.context,"itemView",Toast.LENGTH_SHORT).show()
+        }
+        viewHolder.fruitImager.setOnClickListener{
+            Toast.makeText(parent.context,"imager",Toast.LENGTH_SHORT).show()
+        }
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
